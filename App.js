9ef895dfcec5bad9b4588ad006584e7a304b6a7f
Bassam Awad <benawad97@gmail.com>
2018-01-02 14:09:03 -0600

    build the layout

---
+++ App.js
@@ -0,0 +1,75 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+import Button from './Button';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  top: {
+    paddingTop: 20,
+  },
+  bottom: {
+    flex: 1,
+  },
+  number: {
+    color: '#fff',
+    backgroundColor: '#424242',
+    textAlign: 'right',
+    padding: 10,
+    fontSize: 30,
+    fontWeight: 'bold',
+    borderBottomWidth: 1,
+    borderColor: '#fff',
+  },
+  row: {
+    flex: 1,
+    flexDirection: 'row',
+    borderBottomWidth: 1,
+    borderColor: '#fff',
+  },
+});
+
+const App = () => (
+  <View style={styles.container}>
+    <View style={styles.top}>
+      <Text style={styles.number}>0</Text>
+      <Text style={styles.number}>0</Text>
+      <Text style={styles.number}>0</Text>
+    </View>
+    <View style={styles.bottom}>
+      <View style={styles.row}>
+        <Button text="clear" />
+        <Button text="pow" />
+        <Button text="swap" />
+        <Button text="/" />
+      </View>
+      <View style={styles.row}>
+        <Button text="9" />
+        <Button text="8" />
+        <Button text="7" />
+        <Button text="X" />
+      </View>
+      <View style={styles.row}>
+        <Button text="6" />
+        <Button text="5" />
+        <Button text="4" />
+        <Button text="-" />
+      </View>
+      <View style={styles.row}>
+        <Button text="3" />
+        <Button text="2" />
+        <Button text="1" />
+        <Button text="+" />
+      </View>
+      <View style={styles.row}>
+        <Button text="0" />
+        <Button text="." />
+        <Button text="enter" special />
+      </View>
+    </View>
+  </View>
+);
+
+export default App;
---
+++ Button.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { StyleSheet, View, Text } from 'react-native';
+
+const baseContainer = {
+  alignItems: 'center',
+  justifyContent: 'center',
+  borderRightWidth: 1,
+  borderColor: '#fff',
+};
+
+const baseText = {
+  fontSize: 36,
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fafafa',
+    ...baseContainer,
+  },
+  specialContainer: {
+    flex: 2,
+    backgroundColor: '#9bc23c',
+    ...baseContainer,
+  },
+  text: baseText,
+  specialText: {
+    ...baseText,
+    color: '#fff',
+  },
+});
+
+const Button = ({ text, special }) => (
+  <View style={special ? styles.specialContainer : styles.container}>
+    <Text style={special ? styles.specialText : styles.text}>{text}</Text>
+  </View>
+);
+
+export default Button;
