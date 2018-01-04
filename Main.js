517dd4aac8bea9bb9a4f0ff3ce8d556430f21fb2
Bassam Awad <benawad97@gmail.com>
2018-01-04 12:10:54 -0600

    handle operations

--- Main.js
+++ Main.js
@@ -3,8 +3,17 @@ import { StyleSheet, Text, View } from 'react-native';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 
-import { pressNum, enter } from './modules';
+import { pressNum, enter, operation } from './modules';
 import Button from './Button';
+
+const baseNumber = {
+  backgroundColor: '#333',
+  textAlign: 'right',
+  padding: 10,
+  fontSize: 40,
+  borderBottomWidth: 1,
+  borderColor: '#fff',
+};
 
 const styles = StyleSheet.create({
   container: {
@@ -16,15 +25,17 @@ const styles = StyleSheet.create({
   bottom: {
     flex: 1,
   },
-  number: {
+  append: {
     color: '#fff',
-    backgroundColor: '#424242',
-    textAlign: 'right',
-    padding: 10,
-    fontSize: 30,
-    fontWeight: 'bold',
-    borderBottomWidth: 1,
-    borderColor: '#fff',
+    ...baseNumber,
+  },
+  replace: {
+    color: '#2E71E5',
+    ...baseNumber,
+  },
+  push: {
+    color: '#9bc23c',
+    ...baseNumber,
   },
   row: {
     flex: 1,
@@ -34,41 +45,46 @@ const styles = StyleSheet.create({
   },
 });
 
-const App = ({ calculatorState: { stack, inputState }, pressNumWithDispatch, enterAction }) => (
+const App = ({
+  calculatorState: { stack, inputState },
+  operationAction,
+  pressNumWithDispatch,
+  enterAction,
+}) => (
   <View style={styles.container}>
     <View style={styles.top}>
-      <Text style={styles.number}>{stack[2] || 0}</Text>
-      <Text style={styles.number}>{stack[1] || 0}</Text>
-      <Text style={styles.number}>{stack[0] || 0}</Text>
+      <Text style={styles.append}>{stack[2] || 0}</Text>
+      <Text style={styles.append}>{stack[1] || 0}</Text>
+      <Text style={styles[inputState]}>{stack[0] || 0}</Text>
     </View>
     <View style={styles.bottom}>
       <View style={styles.row}>
         <Button text="clear" />
-        <Button text="pow" />
+        <Button text="pow" onPress={operationAction} />
         <Button text="swap" />
-        <Button text="/" />
+        <Button text="/" onPress={operationAction} />
       </View>
       <View style={styles.row}>
         <Button text="9" onPress={pressNumWithDispatch} />
         <Button text="8" onPress={pressNumWithDispatch} />
         <Button text="7" onPress={pressNumWithDispatch} />
-        <Button text="X" />
+        <Button text="X" onPress={operationAction} />
       </View>
       <View style={styles.row}>
         <Button text="6" onPress={pressNumWithDispatch} />
         <Button text="5" onPress={pressNumWithDispatch} />
         <Button text="4" onPress={pressNumWithDispatch} />
-        <Button text="-" />
+        <Button text="-" onPress={operationAction} />
       </View>
       <View style={styles.row}>
         <Button text="3" onPress={pressNumWithDispatch} />
         <Button text="2" onPress={pressNumWithDispatch} />
         <Button text="1" onPress={pressNumWithDispatch} />
-        <Button text="+" />
+        <Button text="+" onPress={operationAction} />
       </View>
       <View style={styles.row}>
         <Button text="0" onPress={pressNumWithDispatch} />
-        <Button text="." />
+        <Button text="." onPress={pressNumWithDispatch} />
         <Button text="enter" onPress={enterAction} special />
       </View>
     </View>
@@ -82,6 +98,7 @@ export default connect(
       {
         pressNumWithDispatch: pressNum,
         enterAction: enter,
+        operationAction: operation,
       },
       dispatch,
     ),
--- modules.js
+++ modules.js
@@ -9,6 +9,7 @@
 
 const PRESS_NUM = 'PRESS_NUM';
 const ENTER = 'ENTER';
+const OPERATION = 'OPERATION';
 
 // action
 export const pressNum = n => ({
@@ -20,6 +21,11 @@ export const enter = () => ({
   type: ENTER,
 });
 
+export const operation = op => ({
+  type: OPERATION,
+  payload: op,
+});
+
 // inputState = append | replace | push
 
 // a = [1, 3];
@@ -27,8 +33,32 @@ export const enter = () => ({
 // c = [...a, ...b];
 // c = [1, 3, 2];
 
+const doOperation = (x, y, op) => {
+  const a = parseFloat(x);
+  const b = parseFloat(y);
+  if (op === 'pow') {
+    return b ** a;
+  } else if (op === '+') {
+    return b + a;
+  } else if (op === '-') {
+    return b - a;
+  } else if (op === 'X') {
+    return b * a;
+  } else if (op === '/') {
+    return b / a;
+  }
+
+  return 0;
+};
+
 export const reducer = (state = { stack: [], inputState: 'replace' }, { type, payload }) => {
   switch (type) {
+    case OPERATION:
+      return {
+        stack: [`${doOperation(state.stack[0], state.stack[1], payload)}`, ...state.stack.slice(2)],
+        inputState: 'push',
+      };
+      break;
     case ENTER:
       return {
         stack: [state.stack[0] || '0', ...state.stack],
@@ -43,6 +73,11 @@ export const reducer = (state = { stack: [], inputState: 'replace' }, { type, pa
       } else if (state.inputState === 'replace') {
         return {
           stack: [payload, ...state.stack.slice(1)],
+          inputState: 'append',
+        };
+      } else if (state.inputState === 'push') {
+        return {
+          stack: [payload, ...state.stack],
           inputState: 'append',
         };
       }
